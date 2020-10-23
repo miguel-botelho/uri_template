32372539513effdcd5936082d6c21d88d51016bf
Miguel Botelho <jmbotelho95@gmail.com>
2020-10-23 10:18:44 +0100

    Fix remainder of circular dependencies

--- lib/uri_template/rfc6570/expression/unnamed.rb
+++ lib/uri_template/rfc6570/expression/unnamed.rb
@@ -21,8 +21,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require 'uri_template/rfc6570'
-
 class URITemplate::RFC6570::Expression::Unnamed < URITemplate::RFC6570::Expression
 
   def self_pair(_, value, max_length = 0,&block)
